d5c2bf6769e6ce4a808777f477cdc81c47852ba3
rob barnsley <r.barnsley@skatelescope.org>
2023-08-15 12:26:28 +0000

    Update init.sh

--- etc/docker/init.sh
+++ etc/docker/init.sh
@@ -19,7 +19,7 @@ env
 
 # set the root path for openapi docs (https://fastapi.tiangolo.com/advanced/behind-a-proxy/)
 # this should match any proxy path redirect
-cmd="server:app --host "0.0.0.0" --port 8080 --reload --reload-dir ../db/ --reload-dir ../rest/ --reload-dir ../common/ --reload-dir ../../../etc/ --reload-include *.json"
+cmd="server:app --host "0.0.0.0" --port 8080 --reload --reload-dir ../client/ --reload-dir ../db/ --reload-dir ../rest/ --reload-dir ../common/ --reload-dir ../../../etc/ --reload-include *.json"
 if [ ! -z "API_ROOT_PATH" -a "$API_ROOT_PATH" != "" ]; then
   cmd+=' --root-path '$API_ROOT_PATH
 fi
