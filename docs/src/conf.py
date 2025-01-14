42592fc8b4d938a8d5d706c176a380d616a154ba
bajareshraddha <shraddhab.skao@gmail.com>
2025-01-14 12:59:16 +0000

    MAG-1043: Changes for autoapi dir path

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -1,6 +1,11 @@
 import os
 import sys
 
+sys.path.insert(0, os.path.abspath("../../src"))
+sys.path.insert(1, os.path.abspath("../.."))
+sys.path.insert(2, os.path.abspath("../../tests"))
+
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -13,21 +18,60 @@ project = 'ska-src-site-capabilities-api'
 copyright = '2024, Rob Barnsley'
 author = 'Rob Barnsley'
 
+# The short X.Y version
+version = "1.0"
+# The full version, including alpha/beta/rc tags
+release = "1.0"
+
+autodoc_typehints = "none"
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinxcontrib.mermaid', 'sphinxcontrib.plantuml', 'myst_parser', 'autoapi.extension']
+extensions = ['sphinxcontrib.mermaid', 'sphinxcontrib.plantuml', 'myst_parser', 'autoapi.extension', "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.doctest",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.intersphinx",
+    "sphinx_autodoc_typehints",
+]
+autodoc_typehints = "none"
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+autodoc_typehints = "none"
+
+# The master toctree document.
+master_doc = "index"
+ 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
-html_css_files = [
-    'css/custom.css',
+html_theme = 'ska_ser_sphinx_theme'
+# html_static_path = ['_static']
+# html_css_files = [
+#     'css/custom.css',
+# ]
+html_context = {}
+exclude_patterns = []
+
+
+# -- Options for HTMLHelp output ---------------------------------------------
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = "SKASRCSiteCapabilitiesAPIdoc"
+
+
+# -- Options for LaTeX output ------------------------------------------------
+
+latex_elements = {}
+
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title,
+#  author, documentclass [howto, manual, or own class]).
+latex_documents = [
+    (master_doc, "SKASRCSiteCapabilitiesAPIdoc.tex", "SKASRCSiteCapabilitiesAPIdoc Documentation", "manual"),
 ]
 
 # -- Options for MD ----------------------------------------------------------
