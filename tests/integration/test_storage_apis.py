c3f5412109bae434bd8fd6df1eca89c3de19c319
bajareshraddha <shraddhab.skao@gmail.com>
2025-01-24 11:22:49 +0000

    MAG-1030: Updated the assertions

--- tests/integration/test_storage_apis.py
+++ tests/integration/test_storage_apis.py
@@ -17,14 +17,14 @@ def test_list_all_storages():
         f"http://core.{KUBE_NAMESPACE}.svc.{CLUSTER_DOMAIN}:8080/v1/storages"
     )
     response_data = response.json()
-    print(response_data)
-    assert 0
+    for item in response_data:
+        assert item["storages"][0]["id"] != ""
 
 
 @pytest.mark.post_deployment
 def test_get_storage_from_id():
-    """Test to get strorage from strorage id"""
-    storage_id = "19497bf6-0710-4000-8f0b-4d7163ba7801"
+    """Test to get strorage from storage id"""
+    storage_id = "89ee6cac-0977-425e-b766-780a8e14420d"
     response = httpx.get(
         f"http://core.{KUBE_NAMESPACE}.svc.{CLUSTER_DOMAIN}:8080/v1/compute/{storage_id}"
     )
@@ -34,22 +34,36 @@ def test_get_storage_from_id():
 
 
 @pytest.mark.post_deployment
-def test_list_all_grafana_storages():
-    """Test to list all grafana format storages"""
+def test_fail_to_get_storage_from_id():
+    """Test fail to get storage from storage id"""
+    storage_id = "19497bf6-0710-4000-8f0b-4d7163ba7801"
+    response = httpx.get(
+        f"http://core.{KUBE_NAMESPACE}.svc.{CLUSTER_DOMAIN}:8080/v1/compute/{storage_id}"
+    )
+    response_data = response.json()
+    assert (
+        f"Compute element with identifier {storage_id} could not be found"
+        in response_data["detail"]
+    )
+
+
+@pytest.mark.post_deployment
+def test_list_all_storages_for_grafana():
+    """Test to list all storages grafana format"""
     response = httpx.get(
         f"http://core.{KUBE_NAMESPACE}.svc.{CLUSTER_DOMAIN}:8080/v1/storages/grafana"
     )
     response_data = response.json()
-    print(response_data)
-    assert 0
+    for item in response_data:
+        storage_format = item.keys()
+        assert list(storage_format) == ["key", "latitude", "longitude", "name"]
 
 
 @pytest.mark.post_deployment
 def test_list_storages_in_topojson():
-    """Test to list all topojson format storages"""
+    """Test to list all storages topojson format"""
     response = httpx.get(
         f"http://core.{KUBE_NAMESPACE}.svc.{CLUSTER_DOMAIN}:8080/v1/storages/topojson"
     )
     response_data = response.json()
-    print(response_data)
-    assert 0
+    assert response_data["type"] == "Topology"
