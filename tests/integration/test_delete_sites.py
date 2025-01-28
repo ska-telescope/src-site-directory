f49676fdd184d585c3cce01b83168185b52f63af
bajareshraddha <shraddhab.skao@gmail.com>
2025-01-28 12:07:18 +0000

    MAG-1037: Added test helper modules and test data

--- tests/integration/test_delete_sites.py
+++ tests/integration/test_delete_sites.py
@@ -1,10 +1,17 @@
 """
 A module with integration tests for SKA SRC Site Capabilities Site Delete APIs.
 """
+import json
 import os
+import uuid
+from datetime import datetime
 
 import httpx
 import pytest
+from starlette.config import Config
+
+from ska_src_site_capabilities_api.db.backend import MongoBackend
+from tests.resources.common_utils import get_test_json
 
 KUBE_NAMESPACE = os.getenv("KUBE_NAMESPACE")
 CLUSTER_DOMAIN = os.getenv("CLUSTER_DOMAIN")
@@ -41,15 +48,58 @@ def check_sites_availability():
 
 def tear_down():
     """Make sites available again"""
-    response = httpx.post(
-        f"http://core.{KUBE_NAMESPACE}.svc.{CLUSTER_DOMAIN}:8080/v1/sites"
-    )
-    response_data = response.json()
-    print(response_data)
-    if os.getenv("DISABLE_AUTH") == "yes":
-        assert response.status_code == 200
-        assert len(response_data) != 0
-        assert 0
-    else:
-        assert response.status_code == 403
-        assert "Not authenticated" in response_data["detail"]
+    # response = httpx.post(
+    #     f"http://core.{KUBE_NAMESPACE}.svc.{CLUSTER_DOMAIN}:8080/v1/sites"
+    # )
+    # response_data = response.json()
+    # print(response_data)
+    # if os.getenv("DISABLE_AUTH") == "yes":
+    #     assert response.status_code == 200
+    #     assert len(response_data) != 0
+    #     assert 0
+    # else:
+    #     assert response.status_code == 403
+    #     assert "Not authenticated" in response_data["detail"]
+    json_str = get_test_json("sites")
+    print(json_str)
+    make_sites_available(values=json_str)
+
+
+config = Config(".env")
+
+# Instantiate the backend api.
+#
+BACKEND = MongoBackend(
+    mongo_username=config.get("MONGO_USERNAME"),
+    mongo_password=config.get("MONGO_PASSWORD"),
+    mongo_host=config.get("MONGO_HOST"),
+    mongo_port=config.get("MONGO_PORT"),
+    mongo_database=config.get("MONGO_DATABASE"),
+)
+
+
+def make_sites_available(values: json):
+    """Make sites available again"""
+    values = json.loads(values.decode("utf-8"))
+    values["created_at"] = datetime.now().isoformat()
+    values["created_by_username"] = "test-admin"
+
+    # autogenerate ids for id keys
+    def recursive_autogen_id(
+        data, autogen_keys=["id"], placeholder_value="to be assigned"
+    ):
+        if isinstance(data, dict):
+            for key, value in data.items():
+                if key in autogen_keys:
+                    if value == placeholder_value:
+                        data[key] = str(uuid.uuid4())
+                elif isinstance(value, (dict, list)):
+                    data[key] = recursive_autogen_id(value)
+        elif isinstance(data, list):
+            for i in range(len(data)):
+                data[i] = recursive_autogen_id(data[i])
+        return data
+
+    values = recursive_autogen_id(values)
+    print(values)
+    BACKEND.add_site(values)
---
+++ tests/resources/common_utils.py
@@ -0,0 +1,23 @@
+"""This module implement common utils
+"""
+
+from os.path import dirname, join
+
+
+def get_test_json(slug):
+    """
+    Args:
+        slug (str): base name of file
+    Return:
+        Read and return content of file
+    """
+    file_path = join(
+        dirname(__file__),
+        "..",
+        "test-data",
+        f"{slug}.json",
+    )
+    with open(file_path, "r", encoding="UTF-8") as f:
+        json_value = f.read()
+    print(json_value)
+    return json_value
