7929d1b63edbcfc3accbf6e55c9d6f108289f061
ajm-ska <anthony.moraghan@manchester.ac.uk>
2025-03-31 09:53:32 +0100

    adding list_nodes, dump_nodes, and all missing get_ filters

--- src/ska_src_site_capabilities_api/client/site_capabilities.py
+++ src/ska_src_site_capabilities_api/client/site_capabilities.py
@@ -108,6 +108,65 @@ class SiteCapabilitiesClient:
         return resp
 
     @handle_client_exceptions
+    def get_node_version(self, node_name: str, node_version: str = "latest"):
+        """Get description of a node version.
+
+        :param str node_name: The unique name of the node.
+        :param str node_version: The version of the node (default to "latest").
+
+        :return: A requests response.
+        :rtype: requests.models.Response
+        """
+        endpoint = f"{self.api_url}/nodes/{node_name}"
+        params = {"node_version": node_version}
+        resp = self.session.get(endpoint, params=params)
+        resp.raise_for_status()
+        return resp
+
+    @handle_client_exceptions
+    def get_site_from_node_version(self, node_name: str, site_name: str, node_version: str = "latest"):
+        """Get description of a site from a specific node version.
+
+        :param str node_name: The name of the node.
+        :param str site_name: The name of the site associated with the node.
+        :param str node_version: The version of the node (default to "latest").
+
+        :return: A requests response.
+        :rtype: requests.models.Response
+        """
+        endpoint = f"{self.api_url}/nodes/{node_name}/sites/{site_name}"
+        params = {"node_version": node_version}
+        resp = self.session.get(endpoint, params=params)
+        resp.raise_for_status()
+        return resp
+
+    @handle_client_exceptions
+    def get_site_from_id(self, site_id: str):
+        """Get description of a site from an identifier.
+
+        :param str site_id: The unique identifier of the site.
+
+        :return: A requests response.
+        :rtype: requests.models.Response
+        """
+        endpoint = f"{self.api_url}/sites/{site_id}"
+        resp = self.session.get(endpoint)
+        resp.raise_for_status()
+        return resp
+
+    @handle_client_exceptions
+    def dump_nodes(self):
+        """Dump all information about all available nodes.
+
+        :return: A requests response.
+        :rtype: requests.models.Response
+        """
+        endpoint = f"{self.api_url}/nodes/dump"
+        resp = self.session.get(endpoint)
+        resp.raise_for_status()
+        return resp
+
+    @handle_client_exceptions
     def health(self):
         """Get API health.
 
@@ -138,6 +197,26 @@ class SiteCapabilitiesClient:
             "include_inactive": include_inactive,
         }
         resp = self.session.get(compute_endpoint, params=params)
+        resp.raise_for_status()
+        return resp
+
+    @handle_client_exceptions
+    def list_nodes(
+        self,
+        only_names: bool = False,
+        include_inactive: bool = False,
+    ):
+        """List nodes with an option to return only node names.
+
+        :return: A requests response.
+        :rtype: requests.models.Response
+        """
+        nodes_endpoint = "{api_url}/nodes".format(api_url=self.api_url)
+        params = {
+            "only_names": only_names,
+            "include_inactive": include_inactive,
+        }
+        resp = self.session.get(nodes_endpoint, params=params)
         resp.raise_for_status()
         return resp
 
@@ -365,6 +444,17 @@ class SiteCapabilitiesClient:
             "include_inactive": include_inactive,
         }
         resp = self.session.get(storage_areas_topojson_endpoint, params=params)
+        resp.raise_for_status()
+        return resp
+
+    def list_storage_area_types(self):
+        """List storage area types.
+
+        :return: A requests response.
+        :rtype: requests.models.Response
+        """
+        endpoint = f"{self.api_url}/storage-areas/types"
+        resp = self.session.get(endpoint)
         resp.raise_for_status()
         return resp
 
