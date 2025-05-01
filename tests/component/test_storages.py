eabe7239f110da13f13212cccc077cc8479a3141
Shraddha Bajare <shraddha.bajare@skao.int>
2025-05-01 12:52:31 +0100

    MAG-1128: Added disable storages and compute endpoints component tests

--- tests/component/test_storages.py
+++ tests/component/test_storages.py
@@ -22,7 +22,7 @@ def test_list_storages():
 
 @pytest.mark.post_deployment
 def test_set_storages_enabled():
-    """Test to set storage as enabled/disabled"""
+    """Test to set storage as enabled"""
     storage_id = "180f2f39-4548-4f11-80b1-7471564e5c05"
     response = httpx.put(
         f"http://core.{KUBE_NAMESPACE}.svc.{CLUSTER_DOMAIN}:8080/v1/storages/{storage_id}/enabled"  # noqa: E231
@@ -32,5 +32,23 @@ def test_set_storages_enabled():
     if os.getenv("DISABLE_AUTHENTICATION") == "yes":
         assert response.status_code == 200
         assert response_data["storageID"] == storage_id
+        assert response_data["enabled"] is True
+    else:
+        assert response.status_code == 403
+
+
+@pytest.mark.post_deployment
+def test_set_storages_disabled():
+    """Test to set storage as disabled"""
+    storage_id = "180f2f39-4548-4f11-80b1-7471564e5c05"
+    response = httpx.put(
+        f"http://core.{KUBE_NAMESPACE}.svc.{CLUSTER_DOMAIN}:8080/v1/storages/{storage_id}/disabled"  # noqa: E231
+    )
+    response_data = response.json()
+    print(response_data)
+    if os.getenv("DISABLE_AUTHENTICATION") == "yes":
+        assert response.status_code == 200
+        assert response_data["storageID"] == storage_id
+        assert response_data["enabled"] is False
     else:
         assert response.status_code == 403
