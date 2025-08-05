e740778c80b290608e9d0236f450edd02eb52f34
Rob Barnsley <rob.barnsley@skao.int>
2025-08-05 15:33:22 +0100

    removing redundant component tests

--- tests/component/test_storages.py
+++ tests/component/test_storages.py
@@ -19,35 +19,3 @@ def test_list_storages():
         assert response.status_code == 200
     else:
         assert response.status_code == 403
-
-
-@pytest.mark.component
-def test_set_storages_enabled():
-    """Test to set storage as enabled"""
-    storage_id = "180f2f39-4548-4f11-80b1-7471564e5c05"
-    response = httpx.put(f"http://core.{KUBE_NAMESPACE}.svc.{CLUSTER_DOMAIN}:8080/v1/storages/{storage_id}/enable")  # noqa: E231
-    get_storages = httpx.get(f"http://core.{KUBE_NAMESPACE}.svc.{CLUSTER_DOMAIN}:8080/v1/storages/{storage_id}")  # noqa: E231
-    get_storage_response_data = get_storages.json()
-    print(get_storage_response_data)
-    if os.getenv("DISABLE_AUTHENTICATION") == "yes":
-        assert response.status_code == 200
-        assert get_storage_response_data["id"] == storage_id
-        assert get_storage_response_data["is_force_disabled"] is False
-    else:
-        assert response.status_code == 403
-
-
-@pytest.mark.component
-def test_set_storages_disabled():
-    """Test to set storage as disabled"""
-    storage_id = "180f2f39-4548-4f11-80b1-7471564e5c05"
-    response = httpx.put(f"http://core.{KUBE_NAMESPACE}.svc.{CLUSTER_DOMAIN}:8080/v1/storages/{storage_id}/disable")  # noqa: E231
-    get_storage = httpx.get(f"http://core.{KUBE_NAMESPACE}.svc.{CLUSTER_DOMAIN}:8080/v1/storages/{storage_id}")  # noqa: E231
-    get_storage_response_data = get_storage.json()
-    print(get_storage_response_data)
-    if os.getenv("DISABLE_AUTHENTICATION") == "yes":
-        assert response.status_code == 200
-        assert get_storage_response_data["id"] == storage_id
-        assert get_storage_response_data["is_force_disabled"] is True
-    else:
-        assert response.status_code == 403
