a3d77204ad5aa0333b4f692a4834526e3349b473
MicheleDelliVeneri <micheledelliveneri@gmail.com>
2025-08-12 17:53:39 +0100

    fix:
    - changed dp to use the nginx proxy mediated url
    
    - changed tests to use relative path

--- tests/integration/test_site_capabilities_api.py
+++ tests/integration/test_site_capabilities_api.py
@@ -20,21 +20,24 @@ BASE_API_URL = "http://scapi-core:8080/v1"
 @pytest.fixture(scope="module")
 def compute_storm2():
     """Fixture to return storm2 compute json."""
-    with Path("tests/assets/integration/compute_storm2.json").open("r") as compute_file:
+    assets_dir = (Path(__file__).resolve().parent.parent / "assets" / "integration")
+    with (assets_dir / "compute_storm2.json").open("r", encoding="utf-8") as compute_file:
         return json.load(compute_file)
 
 
 @pytest.fixture(scope="module")
 def node_storm1():
     """Fixture to return storm1 node json."""
-    with Path("tests/assets/integration/node_storm1.json").open("r") as node_file:
+    assets_dir = (Path(__file__).resolve().parent.parent / "assets" / "integration")
+    with (assets_dir / "node_storm1.json").open("r", encoding="utf-8") as node_file:
         return json.load(node_file)
 
 
 @pytest.fixture(scope="module")
 def node_storm2():
     """Fixture to return storm2 node json."""
-    with Path("tests/assets/integration/node_storm2.json").open("r") as node_file:
+    assets_dir = (Path(__file__).resolve().parent.parent / "assets" / "integration")
+    with (assets_dir / "node_storm2.json").open("r", encoding="utf-8") as node_file:
         return json.load(node_file)
 
 
