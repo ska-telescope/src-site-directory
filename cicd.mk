389f5394a4f7bdd35cffd92fb8a3c981b9400990
Rob Barnsley <Rob.Barnsley@SKAO.int>
2025-09-24 15:22:16 +0000

    fix cicd

--- cicd.mk
+++ cicd.mk
@@ -1,7 +1,7 @@
 # Bespoke chart configuration for deployment in CI/CD context (k8s.mk)
 K8S_CHART_PARAMS += $(K8S_CHART_COMMON_PARAMS) \
-	--set svc.api.image.image=scapi-core \
-	--set svc.api.image.registry=registry.gitlab.com/ska-telescope/src/src-service-apis/$(PROJECT) \
+	--set svc.api.image.image=$(PROJECT)-core \
+	--set svc.api.image.registry=registry.gitlab.com/ska-telescope/src/src-service-apis/$(CI_PROJECT_NAME) \
 	--set svc.api.image.tag=$(VERSION)-dev.c$(CI_COMMIT_SHORT_SHA) \
 	--set persistence.storageClass=bds1 \
 	--set svc.api.disable_authentication=$(DISABLE_AUTHENTICATION) \
@@ -11,6 +11,7 @@ K8S_CHART_PARAMS += $(K8S_CHART_COMMON_PARAMS) \
 # Build additional tag for integration environment (oci.mk)
 OCI_BUILD_ADDITIONAL_TAGS = $(CI_COMMIT_REF_SLUG)
 
+# Override pre for oci-build (oci.mk): Check if branch is behind origin/main
 oci-pre-build-all:
 	@git fetch origin main;
 	@echo "🚀 Checking branch HEAD..."
