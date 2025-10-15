629e8d637cfebcc84f0e8958db9ccf38820a4d97
anujachaitanya <anujachaitanya09@gmail.com>
2025-10-15 18:18:41 +0530

    [NEON-436 | Anuja/Sayan] Extracted reinitialiseForm in downtime.js

--- src/ska_src_site_capabilities_api/rest/static/js/downtime.js
+++ src/ska_src_site_capabilities_api/rest/static/js/downtime.js
@@ -1,31 +1,75 @@
 function loadDynamicOptions(resourceType, values) {
     const siteSelected = $('select[name="site"]').val();
+    const site = values.sites.find(site => site.name === siteSelected);
     let options = {};
+
     switch (resourceType) {
         case 'sites':
-            values.sites.forEach(site => {
-                options[site.id] = site.name;
-            })
-            return options;
-        case 'compute':
-            let site = values.sites.find(site1 => site1.name === siteSelected);
-            site.compute.forEach(compute => {
+            options[site.id] = site.name;
+            break;
+        case 'compute': {
+            site?.compute?.forEach(compute => {
                 options[compute.id] = compute.name || compute.description;
-            })
-            return options;
-        case 'storages':
-            let siteForStorage = values.sites.find(site1 => site1.name === siteSelected);
-            siteForStorage.storages.forEach(storage => {
+            });
+            break;
+        }
+        case 'storages': {
+            site?.storages?.forEach(storage => {
                 options[storage.id] = storage.host;
-            })
-            return options;
-        case 'storage-areas':
-            let siteForStorageArea = values.sites.find(site1 => site1.name === siteSelected);
-            siteForStorageArea.storages.forEach((storage) => {
-                storage.areas.forEach(area => {
+            });
+            break;
+        }
+        case 'storage_areas': {
+            site?.storages?.forEach(storage => {
+                storage.areas?.forEach(area => {
                     options[area.id] = `${storage.host} - ${area.name}`;
                 });
             });
-            return options;
+            break;
+        }
+        case 'compute_local_services': {
+            site?.compute?.forEach(compute => {
+                compute.associated_local_services?.forEach(service => {
+                    options[service.id] = `${compute.name || compute.description} - ${service.type} - ${service.host}`;
+                });
+            });
+            break;
+        }
+        case 'compute_global_services': {
+            site?.compute?.forEach(compute => {
+                compute.associated_global_services?.forEach(service => {
+                    options[service.id] = `${compute.name || compute.description} - ${service.type} - ${service.host}`;
+                });
+            });
+            break;
+        }
     }
+    return options;
+}
+
+function reinitializeForm(resourceType, values, downtime_form, downtimeFormUi) {
+    const options = loadDynamicOptions(resourceType, values);
+    const updatedSchema = {...downtime_schema};
+
+    const currentFormValues = downtime_form.jsonFormValue();
+    updatedSchema.properties.specificResource.enum = Object.keys(options);
+
+    const updatedFormUi = downtimeFormUi.map(item => {
+        if (item.key === "specificResource") {
+            return {...item, titleMap: options};
+        }
+        return item;
+    });
+
+    downtime_form.html('');
+    downtime_form.jsonForm({
+        schema: updatedSchema,
+        form: updatedFormUi,
+        value: currentFormValues,
+        validate: true,
+        onSubmit: function (formErrors, values) {
+            alert("hello")
+            // handle submit
+        }
+    });
 }
