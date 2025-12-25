be76bc80ea75cdc005e2fa9b9b2d8c72b92203cf
MicheleDelliVeneri <micheledelliveneri@gmail.com>
2025-12-25 18:06:02 +0000

    feat: updated context manager to use async with lifespan

--- src/ska_src_site_capabilities_api/rest/server.py
+++ src/ska_src_site_capabilities_api/rest/server.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import time
+from contextlib import asynccontextmanager
 
 from authlib.integrations.requests_client import OAuth2Session
 from fastapi import FastAPI
@@ -23,7 +24,9 @@ from ska_src_site_capabilities_api.rest.routers.schemas import schemas_router
 from ska_src_site_capabilities_api.rest.routers.services import services_router
 from ska_src_site_capabilities_api.rest.routers.sites import sites_router
 from ska_src_site_capabilities_api.rest.routers.status import status_router
-from ska_src_site_capabilities_api.rest.routers.storage_areas import storage_areas_router
+from ska_src_site_capabilities_api.rest.routers.storage_areas import (
+    storage_areas_router,
+)
 from ska_src_site_capabilities_api.rest.routers.storages import storages_router
 
 config = Config(".env")
@@ -32,49 +35,67 @@ config = Config(".env")
 #
 logger = logging.getLogger("uvicorn")
 
-# Instantiate FastAPI app and add some resources that are shared between routers.
-# See (http://starlette.io/applications/#storing-state-on-the-app-instance)
-#
-# The following resources are accessible through request.state.app:
-#
-# - Debug mode i.e. whether to run service unauthenticated (debug)
-# - j2 templates (templates)
-# - Service version (service_version)
-# - Permissions service name and version (permissions_service_name & permissions_service_version)
-# - Common dependencies (common_dependencies)
-# - Permissions based dependencies (permissions_dependencies)
-# - OAuth2 request session for the ska_src_site_capabilities_api client (api_iam_client).
-# - IAM endpoints (iam_endpoints).
-# - Mongo backend (backend).
-# - Authentication client for browser based www/ routes (auth).
-# - Service start time (service_start_time).
-#
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Application lifespan manager.
+
+    Initializes application state and resources on startup.
+    """
+    # Get instance of IAM constants
+    iam_endpoints = constants.IAM(client_conf_url=config.get("IAM_CLIENT_CONF_URL"))
+
+    # Instantiate a Permissions client
+    permissions = PermissionsClient(config.get("PERMISSIONS_API_URL"))
+    permissions_service_name = config.get("PERMISSIONS_SERVICE_NAME")
+    permissions_service_version = config.get("PERMISSIONS_SERVICE_VERSION")
+
+    # Instantiate permissions dependencies
+    permissions_dependencies = dependencies.Permissions(
+        permissions=permissions,
+        permissions_service_name=permissions_service_name,
+        permissions_service_version=permissions_service_version,
+    )
+
+    # Instantiate OAuth2 request session for the ska_src_site_capabilities_api client
+    api_iam_client = OAuth2Session(
+        config.get("API_IAM_CLIENT_ID"),
+        config.get("API_IAM_CLIENT_SECRET"),
+        scope=config.get("API_IAM_CLIENT_SCOPES", default=""),
+    )
+
+    # Instantiate Mongo backend
+    backend = MongoBackend(
+        mongo_username=config.get("MONGO_USERNAME"),
+        mongo_password=config.get("MONGO_PASSWORD"),
+        mongo_host=config.get("MONGO_HOST"),
+        mongo_port=config.get("MONGO_PORT"),
+        mongo_database=config.get("MONGO_DATABASE"),
+    )
+
+    # Instantiate authentication client for browser based www/ routes
+    auth = AuthenticationClient(config.get("AUTH_API_URL"))
+
+    # Store state in app
+    app.state.iam_endpoints = iam_endpoints
+    app.state.permissions_dependencies = permissions_dependencies
+    app.state.api_iam_client = api_iam_client
+    app.state.backend = backend
+    app.state.auth = auth
+
+    yield
+
+
+# Instantiate FastAPI app
+app = FastAPI(lifespan=lifespan)
+
+# Store app state (accessible through request.app.state)
 app.state.debug = config.get("DISABLE_AUTHENTICATION", default=None) == "yes"
 app.state.templates = Jinja2Templates(directory="templates")
 app.state.service_version = os.environ.get("SERVICE_VERSION")
 app.state.permissions_service_name = config.get("PERMISSIONS_SERVICE_NAME")
 app.state.permissions_service_version = config.get("PERMISSIONS_SERVICE_VERSION")
 app.state.common_dependencies = dependencies.Common()
-app.state.permissions_dependencies = dependencies.Permissions(
-    permissions=PermissionsClient(config.get("PERMISSIONS_API_URL")),
-    permissions_service_name=app.state.permissions_service_name,
-    permissions_service_version=app.state.permissions_service_version,
-)
-app.state.api_iam_client = OAuth2Session(
-    config.get("API_IAM_CLIENT_ID"),
-    config.get("API_IAM_CLIENT_SECRET"),
-    scope=config.get("API_IAM_CLIENT_SCOPES", default=""),
-)
-app.state.iam_endpoints = constants.IAM(client_conf_url=config.get("IAM_CLIENT_CONF_URL"))
-app.state.backend = MongoBackend(
-    mongo_username=config.get("MONGO_USERNAME"),
-    mongo_password=config.get("MONGO_PASSWORD"),
-    mongo_host=config.get("MONGO_HOST"),
-    mongo_port=config.get("MONGO_PORT"),
-    mongo_database=config.get("MONGO_DATABASE"),
-)
-app.state.auth = AuthenticationClient(config.get("AUTH_API_URL"))
 app.state.service_start_time = time.time()
 
 # Add CORS middleware. Static mounts must be added later after the versionize() call.
@@ -104,10 +125,14 @@ app.include_router(services_router)
 app.include_router(schemas_router)
 app.include_router(status_router)
 
-# Versionise the API.
-#
-versionizer = Versionizer(app=app, prefix_format="/v{major}")
-versions = versionizer.versionize()
+# Versionize the API
+versions = Versionizer(
+    app=app,
+    prefix_format="/v{major}",
+    semantic_version_format="{major}",
+).versionize()
+
+# Mount static files
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Customise openapi.json.
@@ -117,10 +142,14 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 # - Remove 422 responses.
 #
 for route in app.routes:
-    if isinstance(route.app, FastAPI):  # find any FastAPI subapplications (e.g. /v1/, /v2/, ...)
+    if isinstance(
+        route.app, FastAPI
+    ):  # find any FastAPI subapplications (e.g. /v1/, /v2/, ...)
         subapp = route.app
         subapp.state = app.state  # copy original app state to all subapps
-        subapp_base_path = "{}{}".format(os.environ.get("API_ROOT_PATH", default=""), route.path)
+        subapp_base_path = "{}{}".format(
+            os.environ.get("API_ROOT_PATH", default=""), route.path
+        )
         subapp.openapi()
         subapp.openapi_schema["servers"] = [{"url": subapp_base_path}]
         subapp.openapi_schema["info"]["title"] = "Site Capabilities API Overview"
@@ -174,11 +203,17 @@ for route in app.routes:
                     if attr.get("responses", {}).get("422"):
                         del attr.get("responses")["422"]
                     method = method.strip("/")
-                    sample_template_filename = "{}-{}-{}.j2".format(language, path, method).replace("/", "-")
-                    sample_template_path = os.path.join("request-code-samples", sample_template_filename)
+                    sample_template_filename = "{}-{}-{}.j2".format(
+                        language, path, method
+                    ).replace("/", "-")
+                    sample_template_path = os.path.join(
+                        "request-code-samples", sample_template_filename
+                    )
                     if os.path.exists(sample_template_path):
                         with open(sample_template_path, "r", encoding="utf-8") as f:
                             sample_source_template = f.read()
                         code_samples = attr.get("x-code-samples", [])
-                        code_samples.append({"lang": language, "source": str(sample_source_template)})
+                        code_samples.append(
+                            {"lang": language, "source": str(sample_source_template)}
+                        )
                         attr["x-code-samples"] = code_samples
