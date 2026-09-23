bf9228c3e5e82b1c0baaf7b25493ac57949d320b
Shraddha Bajare <shraddha.bajare@skao.int>
2026-09-23 13:20:13 +0100

    UV-1517: Keep the format, lint variables as is

--- testing.mk
+++ testing.mk
@@ -1,3 +1,9 @@
+PYTHON_LINE_LENGTH=150
+PYTHON_SWITCHES_FOR_ISORT=
+PYTHON_SWITCHES_FOR_BLACK=
+PYTHON_SWITCHES_FOR_FLAKE8=--ignore=F401,F811,F821,W503
+PYTHON_SWITCHES_FOR_PYLINT=--ignore=W503,W0212
+
 # Common configuration items for pytest (python.mk, k8s.mk)
 # The following sets the expected location of the package inside CI & sets the required variables for component testing.
 PYTHON_VARS_BEFORE_PYTEST=PYTHONPATH=.:./src CLUSTER_DOMAIN=$(CLUSTER_DOMAIN) KUBE_NAMESPACE=$(KUBE_NAMESPACE) DISABLE_AUTHENTICATION=$(DISABLE_AUTHENTICATION)
